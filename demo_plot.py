5c0664b372dd32385b1ca9f7f2b68247469f47f4
Davide Pilastro <davide.pilastro@gmail.com>
2023-10-30 22:36:38 +0100

    docs: add documentation and demo plot image

--- demo_plot.py
+++ demo_plot.py
@@ -4,10 +4,17 @@ import pandas as pd
 if __name__ == "__main__":
 
     df = pd.read_csv('cmake-build-release/output.csv', names = ['t', 'y', 'rolling_mean', 'rolling_std_dev'], delimiter=';')
-    fig, ax = plt.subplots(2,1, sharex=True)
-    ax[0].plot(df.t, df.y, color = 'grey')
-    ax[0].plot(df.t, df.rolling_mean, color='black')
-    ax[1].plot(df.t, df.rolling_std_dev, color= 'black')
+    fig, ax = plt.subplots(2, 1, sharex=True)
+    ax[0].plot(df.y, color='grey', label='data')
+    ax[0].plot(df.rolling_mean, color='black', label='rolling mean')
+    ax[0].grid(True)
+    ax[0].legend(loc='lower right')
+    ax[1].plot(df.rolling_std_dev, color='black', label='rolling std-dev')
+    ax[1].grid(True)
+    ax[1].legend(loc='lower right')
+    ax[-1].set_xlabel('Samples #')
+    ax[-1].set_xlim((0, df.shape[0]))
+
 
     plt.show()
 
